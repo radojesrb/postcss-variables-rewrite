aed1f47442eb968b2476290b4329e766b9e739ca
Dusan Radojevic <dusan.radojevic@smava.de>
2017-12-28 21:13:59 +0100

    playing around

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-variables-rewrite', function (opts) {
     opts = opts || {};
-    
+
     return function (css) {
-      css.walk(function (node) {
-        if (node.type === 'atrule' && node.params === '"./variables"') {
-          node.params = '"./variables.' + opts.themeIdentifier + '"';
+      css.walkAtRules(function (node) {
+        if (node.name  === 'import' && node.params === '"./variables"') {
+          node.params = '"./variables-' + opts.themeIdentifier + '"';
         }
       });
     };
